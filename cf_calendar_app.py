53f5916dab97678e343c47d1bea84e36be8c2349
jsowicki <jakub.sowicki@oracle.com>
2019-01-13 19:22:44 +0100

    Created initial version of json_parser for PerfectGym http responses. Allows user to extract classes ID basing on classes date/time and trainer.

--- cf_calendar_app.py
+++ cf_calendar_app.py
@@ -3,5 +3,12 @@ import bot
 if __name__ == "__main__":
     # execute only if run as a script
 
+    classDetails = {
+        "startTime": "07:00",
+        "date": "2019-01-16",
+        "trainer": "Kamil Klich",
+        "title": 'CrossFit'
+    }
+
     perfectGymBot = bot.PerfectGymBot()
-    perfectGymBot.client_login().select_class()
+    perfectGymBot.client_login().select_class(classDetails)
