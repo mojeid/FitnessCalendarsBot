b30e05ddfdcc7a5c50f8c78468319f6722ca3d09
jsowicki <jakub.sowicki@oracle.com>
2019-01-20 18:02:35 +0100

    Introduced config file to incorporate configuration differences between various fitness calendars systems.

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    APP_NAME = 'fitness_calendars_bot'
+
+
+class PlatiniumGymConfig(Config):
+    BASE_URL = 'https://platinium.perfectgym.pl/ClientPortal2'
+    CLASS_DETAILS_REQUEST_METHOD = 'GET'
+
+
+class CFKrakowConfig(Config):
+    BASE_URL = 'https://crossfit.perfectgym.pl/ClientPortal2'
+    CLASS_DETAILS_REQUEST_METHOD = 'POST'
