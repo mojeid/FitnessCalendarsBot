e2a81db0d030e884e5ce52df0017007642b14e49
jsowicki <jakub.sowicki@oracle.com>
2019-01-24 16:52:52 +0100

    Added a functionality to show list of all currently supported fitness networks or supported fitness clubs within given fitness network

--- main/json_parser.py
+++ main/json_parser.py
@@ -1,4 +1,10 @@
 """ Module responsible data from custom PerfectGym app HTTP responses. """
+import json
+import logging
+
+import os
+
+_logger = logging.getLogger("json_parser")
 
 
 def get_class_id_from_perfectgym_classess_list(json_response, class_details):
@@ -23,3 +29,47 @@ def get_class_id_from_perfectgym_classess_list(json_response, class_details):
 
     return class_id
 
+
+def get_supported_fitness_networks():
+    """
+    :return: List of supported fitness networks
+    """
+    json_data = _read_fitness_clubs_json()
+
+    fitness_networks_list = []
+    for network in json_data:
+        fitness_networks_list.append(network.title())
+    return fitness_networks_list
+
+
+def get_list_of_clubs(fitness_network_name):
+    """
+    Shows list of currently supported fitness clubs within given Fitness network like Platinium.
+
+    :param fitness_network_name: E.g. Platinium, Crossfit etc
+    :return: List of clubs available in that network
+    """
+    json_data = _read_fitness_clubs_json()
+    fitness_clubs = []
+
+    try:
+        for club in json_data[fitness_network_name.lower()]:
+            fitness_clubs.append(club['name'])
+    except KeyError:
+        _logger.warning('There is no club with such name. Please provide correct club name')
+        return
+
+    return fitness_clubs
+
+
+def _read_fitness_clubs_json():
+    if 'tests' in os.getcwd():
+        filepath = '../main/resources/fitness_clubs.json'
+    else:
+        filepath = 'main/resources/fitness_clubs.json'
+
+    file = open(filepath)
+    json_data = json.loads(file.read())
+    file.close()
+
+    return json_data
