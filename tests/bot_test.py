bf919afbf8fa1fdbe7592b0c018d2f9e1f826273
jsowicki <jakub.sowicki@oracle.com>
2019-09-10 22:13:31 +0200

    Fixed always passing login unit tests for both PerfectGymBot and EFitnessBot

--- tests/bot_test.py
+++ tests/bot_test.py
@@ -1,17 +1,33 @@
-import configparser
 import unittest
 
 from main import bot, session_builder
 
 
+class GeneralBotTestCase(unittest.TestCase):
+    pass
+
+
 class PerfectGymBotTestCase(unittest.TestCase):
     def setUp(self):
-        config = configparser.ConfigParser()
-        config.read('../main/resources/configuration.ini')
-        self.bot = bot.PerfectGymBot(session=session_builder.Session.build(), config=config['Centrumfitness'])
+        self.bot = bot.PerfectGymBot(session=session_builder.Session.build(), config_name='Centrumfitness')
 
     def test_client_login(self):
-        self.assertIsNotNone(self.bot.login(), "Client Login is not working correctly!")
+        self.bot.login()
+        self.assertTrue(self.bot._is_user_logged_in(), "Client Login is not working correctly!")
 
     def test_get_booked_classes(self):
         self.assertTrue(isinstance(self.bot.get_booked_classes(), list))
+
+
+class EFitnessBotTestCase(unittest.TestCase):
+    def setUp(self):
+        self.bot = bot.EFitnessBot(session=session_builder.Session.build(), config_name='Platinium')
+
+    def test_client_login(self):
+        self.bot.login()
+        self.assertTrue(self.bot._is_user_logged_in(), "Client Login is not working correctly!")
+
+    # def test_get_booked_classes(self):
+    #     self.assertTrue(isinstance(self.bot.get_booked_classes(), list))
+
+## TODO: zrobić osobne klasy testowe dla PerfectGym bota i dla EFitness bota. Oraz osobną wspolna testująca logowanie na wszystkich możliwych konfiguracjach, bez patrzenia jaki to jest bot?
